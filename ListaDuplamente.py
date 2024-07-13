b9b4855e8475a72d3d0ca5c857dffc4e17703b89
Rafael <mouravieira44@gmail.com>
2024-07-13 17:28:50 -0300

    1.05

---
+++ ListaDuplamente.py
@@ -0,0 +1,22 @@
+from NoDuplo import NoDuplo
+
+class ListaDuplamente:
+
+    def __init__(self):
+        self.head = NoDuplo()
+        self.tail = NoDuplo()
+        self.head.prox = self.tail
+        self.tail.ant = self.head
+
+    def inserir(self, dado):
+        NoNovo = NoDuplo(dado)
+        NoNovo.ant = self.head
+        NoNovo.prox = self.tail
+        self.head.prox = NoNovo
+        self.tail.ant = NoNovo
+        
+    def consulta():
+        pass
+
+    def remover():
+        pass
