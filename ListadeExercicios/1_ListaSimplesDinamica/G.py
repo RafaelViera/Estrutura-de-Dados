eff9e5f71aff33364ac7483599bd8ded580f520a
RafaelViera <mouravieira44@gmail.com>
2024-07-13 16:02:53 -0300

    att

--- ListadeExercicios/1_ListaSimplesDinamica/G.py
+++ ListadeExercicios/1_ListaSimplesDinamica/G.py
@@ -1,11 +1,18 @@
 # Retire o elemento de maior valor da lista
 
+# TODO VERIFICAR SE AINDA TEM ALGUM ERRO NO ALGORITMO
+
 def removerMaiorElemeno(self):
     if (self.prim == None):
         return ("A lista está vazia")
-    p = self.prim
+    p = self.prim.prox
+    p_ant = self.prim
     maior = self.prim.chave
-    while (p):
-        if (p.chave > maior):
-            maior = p.chave
-        p = p.prox
+    while (p_ant):
+        if (p_ant.chave > maior):
+            maior = p_ant
+        else:
+            p = p.prox
+            p_ant = p_ant.prox
+    p_ant.prox = maior.prox
+    maior.prox = None
