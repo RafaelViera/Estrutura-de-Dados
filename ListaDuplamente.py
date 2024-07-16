c009abccd94e4ef0c53ed2e513a87f55cbd80a9e
Rafael <mouravieira44@gmail.com>
2024-07-16 11:07:56 -0300

    1.08

--- ListaDuplamente.py
+++ ListaDuplamente.py
@@ -3,21 +3,48 @@ from NoDuplo import NoDuplo
 class ListaDuplamente:
 
     def __init__(self):
-        self.head = NoDuplo()
-        self.tail = NoDuplo()
-        self.head.prox = self.tail
-        self.tail.ant = self.head   
+        self.prim = None
+        self.nelems= 0 
 
     def inserir(self, dado):
-        NoNovo = NoDuplo(dado)
-        p = self.tail.ant         # acho que fazer "self.tail.prox.ant = NoNovo" funcionaria
-        p.prox = NoNovo           # mas coloquei um ponteiro pra ter certeza
-        self.tail.ant = NoNovo  
-        NoNovo.ant = p
-        NoNovo.prox = self.tail
-        
-    def consulta():
-        pass
+        if (self.prim ==  None):
+            No = NoDuplo(dado)
+            self.prim = No
+        p = self.prim
+        while(p):
+            if (self.prim == No):
+                self.prim = NoDuplo(dado)
+            p = p.prox
+            self.nelems = self.nelems + 1
 
-    def remover():
-        pass
+    def consulta(self, dado):
+        if (self.prim == None):
+            return (False)
+        p = self.prim
+        aux = 0
+        while (p):
+            if (p.chave == dado):
+                return (True, aux)
+            p = p.chave
+        return (False)
+
+    def remover(self, dado):
+        if (self.prim == None):
+             return False
+        elif (self.prim.chave == dado):
+             self.prim == self.prim.prox
+             self.nelems -= 1
+             return True
+        else:
+             p = self.prim
+             while (p):
+                if (p.chave == dado):
+                    p.ant.prox = p.prox
+                    p.prox.ant = p.ant
+                    p.prox = None
+                    p.ant = None
+                    self.nelems -= 1
+                    return True
+                p = p.prox
+        return False
+  
